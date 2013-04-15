2de9df5e223b4f715ea11af9cc10d94b8beda797
kimiyoung <kimiyoung@yeah.net>
2013-04-15 16:58:19 +0800

    Retab

--- src/app/demo/influence-maxmization/inf_max2.cpp
+++ src/app/demo/influence-maxmization/inf_max2.cpp
@@ -10,7 +10,7 @@
 
 struct VData {
     bool active;
-	bool signaled;
+    bool signaled;
     float weight;
 };
 
@@ -53,19 +53,19 @@ typedef saedb::sae_graph<VData, EData>  graph_type;
 
 struct double_sum
 {
-	double value;
-	double_sum(double value = 0.0): value(value) {}
-	double_sum operator +=(const double_sum& other)
-	{
-		value += other.value;
-		return *this;
-	}
+    double value;
+    double_sum(double value = 0.0): value(value) {}
+    double_sum operator +=(const double_sum& other)
+    {
+        value += other.value;
+        return *this;
+    }
 };
 
 double_sum DoubleSumAggregator(graph_type::vertex_type& vertex)
 {
-	if (vertex.data().signaled) return double_sum(1.0);
-	return double_sum(0.0);
+    if (vertex.data().signaled) return double_sum(1.0);
+    return double_sum(0.0);
 }
 
 
@@ -165,7 +165,7 @@ public:
         return 0.0;
     }
     void apply(icontext_type& context, vertex_type& vertex, const gather_type& total){
-		vertex.data().signaled = true;
+        vertex.data().signaled = true;
     }
     edge_dir_type scatter_edges(icontext_type& context, const vertex_type& vertex) const{
         return saedb::IN_EDGES;
@@ -196,7 +196,7 @@ public:
         return 0.0;
     }
     void apply(icontext_type& context, vertex_type& vertex, const gather_type& total){
-		vertex.data().signaled = false;
+        vertex.data().signaled = false;
     }
     edge_dir_type scatter_edges(icontext_type& context, const vertex_type& vertex) const{
         return saedb::IN_EDGES;
@@ -229,13 +229,13 @@ void dfs(saedb::IEngine<DFS> *engine, const std::vector<uint32_t> &seedset){
 
 double dfs2(saedb::IEngine<DFS2> *engine, saedb::IEngine<DFS3> *clear_engine, const std::int32_t &seed){
 
-	engine->signalVertex(seed);
-	engine->start();
-	// count the number of signaled vertices.
-	double result = engine->map_reduce_vertices<double_sum>(DoubleSumAggregator).value - 1.0;
+    engine->signalVertex(seed);
+    engine->start();
+    // count the number of signaled vertices.
+    double result = engine->map_reduce_vertices<double_sum>(DoubleSumAggregator).value - 1.0;
 
-	clear_engine->signalVertex(seed);
-	clear_engine->start();
+    clear_engine->signalVertex(seed);
+    clear_engine->start();
 
     // clean
     return result;
@@ -263,7 +263,7 @@ int main(){
     saedb::IEngine<RandomGraph> *random_graph_engine = new saedb::EngineDelegate<RandomGraph>(graph);
     saedb::IEngine<DFS> *dfs_engine = new saedb::EngineDelegate<DFS>(graph);
     saedb::IEngine<DFS2> *dfs2_engine = new saedb::EngineDelegate<DFS2>(graph);
-	saedb::IEngine<DFS3> *clear_engine = new saedb::EngineDelegate<DFS3>(graph);
+    saedb::IEngine<DFS3> *clear_engine = new saedb::EngineDelegate<DFS3>(graph);
     saedb::IEngine<UpdateGraph> *update_graph_engine = new saedb::EngineDelegate<UpdateGraph>(graph);
     
     
@@ -282,55 +282,55 @@ int main(){
     
     std::vector<uint32_t> seed_list;
     
-  	for (int ss = 1; ss <= ss_cnt; ss++) {
-		for (int r = 0; r < R; r++) {
-            //			printGraph();
-			// random a graph
-			randomGraph(random_graph_engine);
-            //			printf("After shuffle");
-			//printGraph();
-			// get the collection of activated nodes by the seeds
-			dfs(dfs_engine, seed_list);
-			
-			for (int i = 0; i < n; i++) {
-				// if node is in the activated node collection, the marginal is set as 0
-                //				printf("current node = %d\n", i);
+      for (int ss = 1; ss <= ss_cnt; ss++) {
+        for (int r = 0; r < R; r++) {
+            //            printGraph();
+            // random a graph
+            randomGraph(random_graph_engine);
+            //            printf("After shuffle");
+            //printGraph();
+            // get the collection of activated nodes by the seeds
+            dfs(dfs_engine, seed_list);
+            
+            for (int i = 0; i < n; i++) {
+                // if node is in the activated node collection, the marginal is set as 0
+                //                printf("current node = %d\n", i);
                 
                 std::cout<< "vertex id:" << graph.vertex(i).id() <<std::endl;
                 if (graph.vertex(i).data().active) {
-					continue;
-				}
-				marginal[i] = dfs2(dfs2_engine, clear_engine, i);
+                    continue;
+                }
+                marginal[i] = dfs2(dfs2_engine, clear_engine, i);
                 std::cout<< "marginal value = " << marginal[i] <<std::endl;
                
-			}
+            }
             
-			cleanGraph(update_graph_engine);
-		}
-		double max_marginal = 0;
-		int seed = -1;
-		for (int i = 0; i < n; i++) {
-			bool flag = false;
-			for (int j = 0; j < seed_list.size(); j++) {
-				if (seed_list[j] == i) {
-					flag = true;
-					break;
-				}
-			}
-			if (flag)
-				continue;
-			marginal[i] /= R;
-			if (max_marginal <= marginal[i]) {
-				max_marginal = marginal[i];
-				seed = i;
-			}
-			marginal[i] = 0;
-		}
-		greedy_s[ss] = max_marginal;
-		seed_list.push_back(seed);
+            cleanGraph(update_graph_engine);
+        }
+        double max_marginal = 0;
+        int seed = -1;
+        for (int i = 0; i < n; i++) {
+            bool flag = false;
+            for (int j = 0; j < seed_list.size(); j++) {
+                if (seed_list[j] == i) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+                continue;
+            marginal[i] /= R;
+            if (max_marginal <= marginal[i]) {
+                max_marginal = marginal[i];
+                seed = i;
+            }
+            marginal[i] = 0;
+        }
+        greedy_s[ss] = max_marginal;
+        seed_list.push_back(seed);
         std::cout<< "current selected seed = " << seed_list[ss-1] << "marginal= "<< greedy_s[ss] << std::endl;
-	
-	}
+    
+    }
     
     
     
@@ -344,7 +344,7 @@ int main(){
     delete random_graph_engine;
     delete dfs_engine;
     delete dfs2_engine;
-	delete clear_engine;
+    delete clear_engine;
     delete update_graph_engine;
 
     return 0;
--- src/app/demo/influence-maxmization/inf_max3.cpp
+++ src/app/demo/influence-maxmization/inf_max3.cpp
@@ -74,67 +74,67 @@ int main(){
 
     double *greedy_s;
     greedy_s = new double[ss_cnt + 1];
-	for (int i = 0 ; i < ss_cnt+1 ; i++){
-		greedy_s[i] = 0;
-	}
+    for (int i = 0 ; i < ss_cnt+1 ; i++){
+        greedy_s[i] = 0;
+    }
 
     int *t;
     t = new int[n];
-	for (int i = 0; i < n; i++) {
-		t[i] = 0;
-	}
+    for (int i = 0; i < n; i++) {
+        t[i] = 0;
+    }
 
     std::vector<int> seed_list;
 
     std::Heap* heap = new std::Heap();
 
-	for (int i = 0; i < n; i++) {
-		heap->push(i, graph.vertex(i).num_in_edges());
-	}
+    for (int i = 0; i < n; i++) {
+        heap->push(i, graph.vertex(i).num_in_edges());
+    }
     heap->print_heap();
     for (int ss = 1; ss <= ss_cnt; ss++) {
 
-		std::pair<double, int> max_marginal = heap->top();
-		int seed_index = max_marginal.second;
-		seed_list.push_back(seed_index);
+        std::pair<double, int> max_marginal = heap->top();
+        int seed_index = max_marginal.second;
+        seed_list.push_back(seed_index);
 
-		greedy_s[ss] =  greedy_s[ss-1]+ (graph.vertex(seed_index).num_in_edges() - t[seed_index])*p;
-		printf("current max marginal is %d, degree = %d, t=%d, value is %lf\n",
+        greedy_s[ss] =  greedy_s[ss-1]+ (graph.vertex(seed_index).num_in_edges() - t[seed_index])*p;
+        printf("current max marginal is %d, degree = %d, t=%d, value is %lf\n",
                max_marginal.second, graph.vertex(seed_index).num_in_edges(), t[seed_index], greedy_s[ss] );
 
-		heap->pop();
+        heap->pop();
         // modify its followees
         std::cout << "number out edges="<< graph.vertex(seed_index).num_out_edges()<<std::endl;
 
         auto e = graph.vertex(seed_index).out_edges();
 
-		for (int i = 0; i < graph.vertex(seed_index).num_out_edges(); i++) {
+        for (int i = 0; i < graph.vertex(seed_index).num_out_edges(); i++) {
 
-			e->MoveTo(i);
+            e->MoveTo(i);
             auto v = e->TargetId();
-			std::cout << "current = " << v << std::endl;
+            std::cout << "current = " << v << std::endl;
 
-			bool flag = false;
-			for (int j = 0; j < seed_list.size(); j++) {
-				if (seed_list[j] == v) {
-					flag = true;
-					break;
-				}
-			}
-			if (flag)
-				continue;
-			t[v]++;
-			int dv = graph.vertex(i).num_in_edges();
-			double marginal = dv - 2 * t[v] - (dv - t[v]) * t[v] * p;
+            bool flag = false;
+            for (int j = 0; j < seed_list.size(); j++) {
+                if (seed_list[j] == v) {
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag)
+                continue;
+            t[v]++;
+            int dv = graph.vertex(i).num_in_edges();
+            double marginal = dv - 2 * t[v] - (dv - t[v]) * t[v] * p;
  //           printf("modify index = %d, dv = %d, tv = %d, p= %lf, marginal= %lf \n", v, dv, t[v], p, dv - 2 * t[v] - (dv - t[v]) * t[v] * p);
-			std::cout << "modify index = " << v << ", dv = " << dv << ", tv = " << t[v] << ", p = " << p << ", marginal = " << dv - 2 * t[v] - (dv - t[v]) * t[v] * p;
-			heap->modify(v, marginal);
-            //			heap->print_heap();
-		}
+            std::cout << "modify index = " << v << ", dv = " << dv << ", tv = " << t[v] << ", p = " << p << ", marginal = " << dv - 2 * t[v] - (dv - t[v]) * t[v] * p;
+            heap->modify(v, marginal);
+            //            heap->print_heap();
+        }
 
-        //		printf("current selected seed = %d, marginal= %lf\n", seed_list[ss - 1],
-        //				greedy_s[ss]);
-	}
+        //        printf("current selected seed = %d, marginal= %lf\n", seed_list[ss - 1],
+        //                greedy_s[ss]);
+    }
 
     for (int i = 1; i <= ss_cnt; i++)
     {
